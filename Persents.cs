d283874442a386da14a29036bcf3c47b11bedffc
Hoshinory <hoshinoryart@gmail.com>
2025-11-12 05:25:59 -0800

    Persents task

---
+++ Persents.cs
@@ -0,0 +1,9 @@
+﻿public static double Calculate(string userInput)
+{
+	var numbers = userInput.Split();
+	var sum = double.Parse(numbers[0]);
+	var months = double.Parse(numbers[2]);
+	var percent = double.Parse(numbers[1]);
+	
+	return sum * Math.Pow((1 + percent / (12 * 100)), months);
+}
