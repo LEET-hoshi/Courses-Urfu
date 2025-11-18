3b60e037c68e6412771990690d120fd5ccd8050b
Hoshinory <hoshinoryart@gmail.com>
2025-11-18 09:04:49 -0800

    pluralize

---
+++ pluralize.cs
@@ -0,0 +1,11 @@
+﻿namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+		if ((count % 100 > 10 && count % 100 < 20) || (count % 10 > 4 && count % 10 < 10) || count == 0 || count % 10 == 0)
+			return "рублей";
+		else return count % 10 == 1 ? "рубль" : "рубля";
+	}
+}
